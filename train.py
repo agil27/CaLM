bf143f9938955eff2e67e70c67606981afbddf2f
vic <wfeng@g.harvard.edu>
2023-11-09 13:48:31 -0500

    update

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ from trl import SFTTrainer
 
 def load_qlora_model_and_tokenizer():
     """
-    Load model and tokenizer
+    Load model and tokenizer with QLoRA
     """
     base_model_name = "NousResearch/Llama-2-7b-hf"
 
@@ -68,7 +68,7 @@ def load_qlora_model_and_tokenizer():
 
 def load_lora_model_and_tokenizer():
     """
-    Load model and tokenizer
+    Load model and tokenizer with LoRA
     """
     base_model_name = "NousResearch/Llama-2-7b-hf"
 
@@ -105,6 +105,46 @@ def load_lora_model_and_tokenizer():
 
     return base_model, tokenizer, peft_config
 
+
+def load_model_and_tokenizer():
+    """
+    Load model and tokenizer
+    """
+    base_model_name = "NousResearch/Llama-2-7b-hf"
+
+
+    # Load model and tokenizer
+    base_model = AutoModelForCausalLM.from_pretrained(
+        base_model_name,
+        use_flash_attention_2=True,
+        torch_dtype=torch.bfloat16,
+        device_map="auto",
+        trust_remote_code=True,
+        token='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'
+        )
+    base_model.config.use_cache = False
+    # More info: https://github.com/huggingface/transformers/pull/24906
+    base_model.config.pretraining_tp = 1
+
+    tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+
+    # LoRA config based on QLoRA paper
+    # peft_config = LoraConfig(
+    #     inference_mode=False,
+    #     lora_alpha=32,
+    #     lora_dropout=0.1,
+    #     r=8,
+    #     bias="none",
+    #     task_type="CAUSAL_LM",
+    # )
+    # base_model = prepare_model_for_kbit_training(base_model)
+    # base_model = get_peft_model(base_model, peft_config)
+    # base_model.print_trainable_parameters()
+
+    return base_model, tokenizer, None
+
 def train(dataset_name, output_dir, batch_size):
     """
     Load dataset
@@ -115,7 +155,8 @@ def train(dataset_name, output_dir, batch_size):
     """
     Prepare model for training
     """
-    model, tokenizer, peft_config = load_lora_model_and_tokenizer()
+    # model, tokenizer, peft_config = load_lora_model_and_tokenizer()
+    model, tokenizer, peft_config = load_model_and_tokenizer()
 
     training_args = TrainingArguments(
         output_dir=output_dir,
