b02b626d3a1dc36d735cff215b35edef1383d407
agil27 <wangyuanbiao2016@gmail.com>
2023-11-11 20:39:52 -0800

    more refactoring about training snapshotting and logging

--- train.py
+++ train.py
@@ -3,36 +3,47 @@
 import os
 import argparse
 import wandb
+from easydict import EasyDict as edict
 
 from datasets import load_dataset
 from transformers import TrainingArguments
 from trl import SFTTrainer
-from utils import load_config
-from models import load_llm_from_huggingface, lora_wrapper, qlora_wrapper
+from utils import load_config, dataset_name_from_dataset_type, run_name_from_config
+from models import (
+    load_llm_from_huggingface,
+    lora_wrapper,
+    qlora_wrapper,
+    default_lora_config,
+)
 
-def train(config):
-    dataset = load_dataset(config.dataset_name, split="train")
+
+def train(config: edict, output_dir: str):
+    dataset = load_dataset(
+        dataset_name_from_dataset_type(config.io.dataset_type), split="train"
+    )
     max_seq_length = 512
     training_args = TrainingArguments(
-        output_dir=config.output_dir,
+        output_dir=output_dir,
         do_train=True,
-        num_train_epochs=config.num_train_epochs,
-        gradient_accumulation_steps=1,
+        num_train_epochs=config.training.num_train_epochs,
+        gradient_accumulation_steps=config.training.gradient_accumulation_steps,
         gradient_checkpointing=True,
         optim="paged_adamw_32bit",
         logging_steps=10,
-        learning_rate=2e-4,
+        learning_rate=config.training.learning_rate,
         bf16=True,
         max_grad_norm=0.3,
         warmup_ratio=0.03,
     )
-    training_args = training_args.set_dataloader(train_batch_size=config.batch_size)
+    training_args = training_args.set_dataloader(
+        train_batch_size=config.training_batch_size
+    )
 
-    model_dict = load_llm_from_huggingface(config.model_name)
-    if config.adapter == "lora":
-        model_dict = lora_wrapper(model_dict, config.lora_config)
-    elif config.adapter == "qlora":
-        model_dict = qlora_wrapper(model_dict, config.lora_config)
+    model_dict = load_llm_from_huggingface(config.training.model_name)
+    if config.model.adapter == "lora":
+        model_dict = lora_wrapper(model_dict, default_lora_config())
+    elif config.model.adapter == "qlora":
+        model_dict = qlora_wrapper(model_dict, default_lora_config())
 
     model = model_dict["model"]
     peft_config = model_dict["lora_config"]
@@ -49,14 +60,21 @@ def train(config):
     )
     trainer.train()
 
-    final_output_dir = os.path.join(config.output_dir, "final_checkpoint")
+    final_output_dir = os.path.join(output_dir, "final_checkpoint")
     trainer.model.save_pretrained(final_output_dir)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", "-c", type=str, required=True, help="Path to the config YAML file.")
+    parser.add_argument(
+        "--config", "-c", type=str, required=True, help="Path to the config YAML file."
+    )
     args = parser.parse_args()
     config = load_config(args.config)
-    wandb.init(name=config.run_name)
-    train(args.dataset, args.checkpoint_dir, args.batchSize)
+    run_name = run_name_from_config(config)
+    wandb.init(name=run_name)
+    output_dir = os.path.join(config.io.output_dir, run_name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    os.copy(args.config, os.path.join(output_dir, "config_snapshot.yaml"))
+    train(config, output_dir)
--- utils.py
+++ utils.py
@@ -1,7 +1,24 @@
 from easydict import EasyDict as edict
 from yaml import safe_load
+import datetime
+
 
 def load_config(config_path: str) -> edict:
     with open(config_path, "r") as f:
         config = safe_load(f)
     return edict(config)
+
+
+def run_name_from_config(config: edict) -> str:
+    formatted_datetime = datetime.now().strftime("%Y%m%d%H%M%S")
+    return f"{config.io.run_tag}-{config.adaptor}-dataset-{config.training.dataset_type}-batch-size-{config.training_batch_size}-epochs-{config.training.num_train_epochs}-{formatted_datetime}"
+
+
+def dataset_name_from_dataset_type(dataset_type: str) -> str:
+    if dataset_type == "science":
+        return "vic0428/imdb-card-pred-science"
+    elif dataset_type == "binary":
+        return "vic0428/imdb-card-pred-binary"
+    elif dataset_type == "decimal":
+        return "vic0428/imdb-card-pred-decimal"
+    raise ValueError(f"Unknown dataset type: {dataset_type}")
