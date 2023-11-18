05ffdedf41d9d98a2de49bb22572a230539c4b3c
agil27 <wangyuanbiao2016@gmail.com>
2023-11-17 21:30:29 -0800

    create a pipeline for text decode

--- inference.py
+++ inference.py
@@ -1,5 +1,7 @@
 import torch
 import re
+from transformers import Pipeline
+from abc import ABC, abstractmethod
 
 
 def qerror(estimated_cardinality, actual_cardinality):
@@ -9,81 +11,63 @@ def qerror(estimated_cardinality, actual_cardinality):
     )
 
 
-class CardinalityInference:
-    """
-    A Virtual Inference class for huggingface LLM cardinality prediction.
-    model: A huggingface model.
-    tokenizer: A huggingface tokenizer
-    """
+def DecodeCardinalityPipeline(Pipeline):
+    def _sanitize_parameters(self, **kwargs):
+        preprocess_kwargs = {}
+        forward_kwargs = {}
+        post_process_kwargs = {}
+        if "max_length" in kwargs:
+            forward_kwargs["max_length"] = kwargs["max_length"]
+        if "decode_mode" in kwargs:
+            post_process_kwargs["decode_mode"] = kwargs["decode_mode"]
+        return preprocess_kwargs, forward_kwargs, post_process_kwargs
 
-    def __init__(self, model, tokenizer, device="cuda:0"):
-        self.model = model
-        self.tokenizer = tokenizer
-        self.device = device if torch.cuda.is_available() else "cpu"
+    def preprocess(self, text, second_text=None):
+        inputs = self.tokenizer(text, return_tensors=self.framework)
+        inputs["prompt"] = text
+        return inputs
 
-    def get_output_text(self, prompt):
-        inputs = self.tokenizer(prompt, return_tensors="pt").to(self.device)
+    def _forward(self, model_inputs, **generate_kwargs):
+        max_length = generate_kwargs.pop("max_length", 512)
         outputs = self.model.generate(
-            input_ids=inputs["input_ids"],
-            attention_mask=inputs["attention_mask"],
+            input_ids=model_inputs["input_ids"],
+            attention_mask=model_inputs["attention_mask"],
             max_new_tokens=50,
             pad_token_id=self.tokenizer.eos_token_id,
         )
-        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)[len(prompt):]
+        output_text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+        return output_text[len(model_inputs["prompt"]) :]
 
-    def inference(self, prompt):
-        pass
+    def postprocess(self, model_outputs, **generate_kwargs):
+        decode_mode = generate_kwargs.pop("decode_mode", "decimal")
+        if decode_mode == "binary":
+            return binary_decode(model_outputs)
+        elif decode_mode == "decimal":
+            return decimal_decode(model_outputs)
+        elif decode_mode == "scientific":
+            return scientific_decode(model_outputs)
+        else:
+            raise ValueError(
+                "Invalid decode mode. Should be one of decimal, binary or scientific."
+            )
+            
 
+def binary_decode(output_text):
+    # A regex pattern that matches the binary format
+    binary_pattern = re.compile(r"([0-1]+)")
 
-class BinaryInference(CardinalityInference):
-    """
-    Run inference and decode with binary format.
-    """
-
-    def __init__(self, model, tokenizer, device="cuda:0"):
-        super(BinaryInference, self).__init__(model, tokenizer, device)
-
-    def decode(self, output_text):
-        # A regex pattern that matches the binary format
-        binary_pattern = re.compile(r"([0-1]+)")
-
-        # Find the matching characters
-        binary_str = re.findall(binary_pattern, output_text)[0]
-        return int(binary_str, 2)
-    
-    def inference(self, prompt):
-        return self.decode(self.get_output_text(prompt))
-
-
-class DecimalInference(CardinalityInference):
-    """
-    Run inference and decode with decimal format
-    """
-    def __init__(self, model, tokenizer, device="cuda:0"):
-        super(DecimalInference, self).__init__(model, tokenizer, device)
-
-    def decode(self, output_text):
-        decimal_pattern = re.compile(r"(\d+)")
-        decimal_str = re.findall(decimal_pattern, output_text)[0]
-        return int(decimal_str)
-
-    def inference(self, prompt):
-        return self.decode(self.get_output_text(prompt))
+    # Find the matching characters
+    binary_str = re.findall(binary_pattern, output_text)[0]
+    return int(binary_str, 2)
 
 
-class ScientificInference(CardinalityInference):
-    """
-    Run inference and decode with scientific format
-    """
-    def __init__(self, model, tokenizer, device="cuda:0"):
-        super(ScientificInference, self).__init__(model, tokenizer, device)
+def decimal_decode(output_text):
+    decimal_pattern = re.compile(r"(\d+)")
+    decimal_str = re.findall(decimal_pattern, output_text)[0]
+    return int(decimal_str)
 
-    def decode(self, output_text):
-        scientific_pattern = re.compile(r"(\d\.\d+e\d+)")
-        scientific_str = re.findall(scientific_pattern, output_text)[0]
-        return int(float(scientific_str))
-    
-    def inference(self, prompt):
-        return self.decode(self.get_output_text(prompt))
 
-        
+def scientific_decode(output_text):
+    scientific_pattern = re.compile(r"(\d\.\d+e\d+)")
+    scientific_str = re.findall(scientific_pattern, output_text)[0]
+    return int(float(scientific_str))
