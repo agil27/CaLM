49fc4eb9411d646fdcfa0f407e452b1c17da971f
agil27 <wangyuanbiao2016@gmail.com>
2023-11-18 21:53:34 -0800

    fix peft model loading in ppo training.:

--- models.py
+++ models.py
@@ -108,21 +108,3 @@ def load_model_from_checkpoint(
         tokenizer.padding_side = "left"
 
     return {"model": model, "tokenizer": tokenizer}
-
-
-def load_trl_model_from_checkpoint(
-    checkpoint_dir: str, use_bf16: bool = True, device_map: str = "auto",
-    pad_left: bool = True
-):
-    model = AutoModelForCausalLM.from_pretrained(
-        checkpoint_dir,
-        device_map=device_map,
-        torch_dtype=torch.bfloat16 if use_bf16 else "auto",
-    )
-    tokenizer = AutoTokenizer.from_pretrained(checkpoint_dir)
-
-    if pad_left:
-        tokenizer.pad_token = tokenizer.bos_token
-        tokenizer.padding_side = "left"
-
-    return {"model": model, "tokenizer": tokenizer}
